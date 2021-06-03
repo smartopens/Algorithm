4ef5cf34f193f652a69506969335b19f01dac7ad
kimhyeonmyeong <44837403+kimhyeonmyeong@users.noreply.github.com>
2021-06-03 23:24:14 +0900

    Add files via upload

---
+++ 그래프/숨바꼭질(BFS).py
@@ -0,0 +1,29 @@
+from collections import deque
+
+n, k = map(int, input().split(' '))
+max = 100001
+
+array = [0] * max
+
+
+def bfs(v):
+    q = deque([v])
+
+    while q:
+        now_pos = q.popleft()
+
+        if now_pos == k:
+            return array[now_pos]
+
+        for next_pos in (now_pos - 1, now_pos + 1, now_pos * 2):
+            if 0 <= next_pos < max and not (array[next_pos]):
+
+                if next_pos == k:
+                    return array[now_pos] + 1
+                else:
+                    array[next_pos] = array[now_pos] + 1
+                    q.append(next_pos)
+
+
+result_time = bfs(n)
+print(result_time)
