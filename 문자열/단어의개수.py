d830b108977fe9b6633e263196d0378a0b640f30
Smart <44837403+kimhyeonmyeong@users.noreply.github.com>
2022-12-11 14:24:39 +0900

    Delete 문자열 directory

--- 문자열/단어의개수.py
+++
@@ -1,7 +0,0 @@
-string = input().split()
-answer = 0
-
-for s in string:
-    answer += 1
-
-print(answer)
--- 문자열/문자열반복.py
+++
@@ -1,8 +0,0 @@
-t = int(input())
-
-for _ in range(t):
-    i, string = map(str, (input().split(' ')))
-    ans = ""
-    for s in string:
-        ans += s*int(i)
-    print(ans)
