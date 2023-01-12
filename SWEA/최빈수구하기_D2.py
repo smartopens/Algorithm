fce31a146ac1382b818737f7de9b0aceb87e78e4
Smart <44837403+kimhyeonmyeong@users.noreply.github.com>
2023-01-12 22:34:59 +0900

    Delete SWEA directory

--- SWEA/최빈수구하기_D2.py
+++
@@ -1,9 +0,0 @@
-t = int(input())
-# Great 9 codes
-for _ in range(t):
-    t_idx = int(input())
-    score_list = list(map(int, input().split()))
-    num_list = [[0,i] for i in range(101)]
-    for i in range(1000): num_list[score_list[i]][0] += 1
-    num_list.sort(key = lambda x: (x[0],x[1]), reverse=True)
-    print("#{} {}".format(t_idx, num_list[0][1]))
